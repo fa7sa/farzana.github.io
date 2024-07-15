5ae228c53607e70402258d83c711a2105bdae111
Farzana Yasmin Ahmad <83102211+fa7sa@users.noreply.github.com>
2024-07-15 18:34:17 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "android development", "web development"],
+    strings: ["Machine Learning", "AI for Science", "High Performance Computing", "High Energy Physics"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
